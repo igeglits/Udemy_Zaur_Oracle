43bb368393d8b9d621507469b57989d42d415cb5
Igor Eglit <igeglits@gmail.com>
2024-01-02 18:10:53 +0000

    lesson 14

--- src/urok14/Timer.java
+++ src/urok14/Timer.java
@@ -2,7 +2,34 @@ package urok14;
 
 class Timer {
     public static void main(String[] args) {
-        timerWithWhile(6);
+        timer3Loops();
+    }
+
+    static void timer3Loops() {
+        int i = 0;
+        OUTER:
+        while (i < 24) {
+            int j = 0;
+            INNER_1:
+            do {
+                if ((i > 1) && j != 0 && (j % 10 == 0)) {
+                    break OUTER;
+                }
+                int s = 0;
+                INNER_2:
+                while (s < 60) {
+                    if ((s * i) > j) {
+                        j++;
+                        continue INNER_1;
+                    }
+                    System.out.println(i + " : " + j + " : " + s);
+                    s++;
+                }
+                j++;
+            }
+            while (j < 60);
+            i++;
+        }
     }
 
     static void timer(int hours) {
@@ -19,18 +46,19 @@ class Timer {
                     if ((s * i) > j) {
                         continue INNER_1;
                     }
-                    System.out.println(i + " : " + j + " : " + s );
+                    System.out.println(i + " : " + j + " : " + s);
                 }
             }
         }
     }
-    static  void timerWithWhile(int hours){
+
+    static void timerWithWhile(int hours) {
         OUTER:
-        for (int chas = 0; chas <hours; chas++){
+        for (int chas = 0; chas < hours; chas++) {
             int minuta = 0;
             INNER:
-            while (minuta<60){
-                System.out.println(chas + ":"+minuta);
+            while (minuta < 60) {
+                System.out.println(chas + ":" + minuta);
                 minuta++;
             }
         }
