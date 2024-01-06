38901b72ca67ab8fdc6a3ef58049f72aad0f466d
Igor Eglit <igeglits@gmail.com>
2024-01-06 21:14:43 +0200

    lesson 16 String methods

--- src/urok16/EmailAddressSubstringing.java
+++ src/urok16/EmailAddressSubstringing.java
@@ -3,22 +3,23 @@ package urok16;
 import java.lang.reflect.Array;
 import java.util.Arrays;
 
-public class EmailAddressSubstringing {
+class EmailAddressSubstringing {
     public static void main(String[] args) {
-        extractEmailDomain(emails);
+        //extractEmailDomain(emails);
+        otZaura(emails);
     }
 
-    public static String emails = "ya123@yahoo.com; on456@mail.ru; ona@gmail.com";
+    public static String emails = "ya123@yahoo.com; on456@mail.ru; ona@gmail.com;";
 
     static void extractEmailDomain(String emails) {
         int[] indexes = new int[6];
-        indexes[5]=emails.length();
+        //indexes[5] = emails.length();
         for (int i = 0, j = 0; i < emails.length(); i++) {
-            if ('@'==(emails.charAt(i))) {
-                indexes[j] = i+1;
+            if ('@' == (emails.charAt(i))) {
+                indexes[j] = i + 1;
                 j++;
             }
-            if (';'==(emails.charAt(i))) {
+            if ('.' == (emails.charAt(i))) {
                 indexes[j] = i;
                 j++;
             }
@@ -26,14 +27,34 @@ public class EmailAddressSubstringing {
         System.out.println(Arrays.toString(indexes));
         System.out.println(emails.length());
         String[] domains = {emails.substring(indexes[0], indexes[1]), emails.substring(indexes[2], indexes[3]), emails.substring(indexes[4], indexes[5])};
-        System.out.println(Arrays.toString(domains));
+        toPrint(domains);
 
-        String firstDomain = emails.substring(indexes[0], indexes[1]);
+        /*String firstDomain = emails.substring(indexes[0], indexes[1]);
         String secondDomain = emails.substring(indexes[2], indexes[3]);
         String thirdDomain = emails.substring(indexes[4], indexes[5]);
         System.out.println(firstDomain + '\n'
                 + secondDomain + '\n'
-                + thirdDomain);
+                + thirdDomain);*/
+    }
+
+    static void toPrint(String[] array) {
+        int i = 0;
+        while (i < array.length){
+            System.out.println(array[i]);
+            i++;
+        }
+    }
+
+    static void otZaura(String string) {
+        int a = 0;
+        int b = 0;
+        int c = 0;
+        while (c < string.length() - 1) {
+            a = string.indexOf('@', c);
+            b = string.indexOf('.', c);
+            c = string.indexOf(';', c + 1);
+            System.out.println(string.substring(a + 1, b));
+        }
     }
 }
 
