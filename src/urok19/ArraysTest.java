33bf7bf112bdabcd759122d1632f536b24eeabb1
Igor Eglit <igeglits@gmail.com>
2024-01-09 16:17:16 +0200

    VarArgs

--- src/urok19/ArraysTest.java
+++ src/urok19/ArraysTest.java
@@ -7,7 +7,7 @@ import static java.util.Arrays.sort;
 
 class ArraysTest {
     static int[] array = {1, 6, -8, -4, 9, 45, -3, -34, 0, 88};
-    static String array2[][] = {{"apple","orange"},{"hello", "buy", "ok"}, {"car"}};
+    static String array2[][] = {{"apple", "orange"}, {"hello", "buy", "ok"}, {"car"}};
 
 
     static void sortirovka(int[] array) {
@@ -32,19 +32,22 @@ class ArraysTest {
             }
             if (i != array.length - 1) {
                 out.print("}, ");
-            }else{
+            } else {
                 out.print("  }");
             }
         }
         out.print(" }");
     }
-    static void array3 (int[] array){
-        int temp=0;
-        for (int i=0 ; i<array.length-1 ; i++){
-            if (array[i]>array[i+1]){
-            temp = array[i+1];
-            array[i+1]=array[i];
-            array[i]= temp;
+
+    static void array3(int[] array) {
+        int temp;
+        for (int i = 0; i < array.length - 2; i++) {
+            for (int j = i + 1; j < array.length - 1; j++) {
+                if (array[i] > array[j]) {
+                    temp = array[j];
+                    array[j] = array[i];
+                    array[i] = temp;
+                }
             }
         }
         out.println();
@@ -52,17 +55,44 @@ class ArraysTest {
             out.print(i + " ");
         }
     }
-    static void array4 (int[] array){
+
+    static void sortDesc(int[] array) {
+        int temp;
+        for (int i = (array.length - 1); i == 1; i--) {
+            for (int j = (array.length - 2); j == 0; j--) {
+                if (array[i] > array[j]) {
+                    temp = array[j];
+                    array[j] = array[i];
+                    array[i] = temp;
+                }
+            }
+        }
+        out.println();
+        for (int i : array) {
+            out.print(i + " ");
+        }
+    }
+
+    static void array4(int[] array) {
         Arrays.stream(array)
                 .sorted().toArray();
-        out.println(Arrays.toString(array));
+        toPrint(array);
+    }
+
+    static void toPrint(int[] array) {
+        for (int i : array) {
+            out.print(i + " ");
+        }
     }
 
     public static void main(String[] args) {
-        sortirovka(array);
-        showArray(array2);
-        showArray(new String[][]{{"man", "woman"}, {"male", "female"}});
-        array3(array);
-        array4(array);
+//        sortirovka(array);
+//        showArray(array2);
+//        showArray(new String[][]{{"man", "woman"}, {"male", "female"}});
+        //      array3(array);
+        //    out.println();
+//        array4(array);
+        out.println("sort Desc");
+        sortDesc(array);
     }
 }
