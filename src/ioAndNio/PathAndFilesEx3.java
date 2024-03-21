3e18d2492dc920411adf97bc621dcaa826a55b91
igor eglit <igeglits@gmail.com>
2024-03-21 16:07:47 +0200

    Paths Files methods

--- src/ioAndNio/PathAndFilesEx3.java
+++ src/ioAndNio/PathAndFilesEx3.java
@@ -4,13 +4,20 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 
 public class PathAndFilesEx3 {
     public static void main(String[] args) throws IOException {
         Path filePath = Paths.get("test17.txt");
-        Files.createFile(filePath);
+        /*Files.createFile(filePath);
         String dialog = "Hello, Igor!\nHow are you?\nI am fine!";
-        Files.write(filePath, dialog.getBytes());
+        Files.write(filePath, dialog.getBytes());*/
+
+        Files.readAllLines(filePath).forEach(System.out::println);
+        List<String> list =  Files.readAllLines(filePath);
+        for (String s : list) {
+            System.out.println(s);
+        }
 
     }
 }
